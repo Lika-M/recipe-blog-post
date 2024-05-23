1b92cf74b7f8219e707483c6530f9b030b7e5592
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-23 18:31:56 +0300

    Added credentials to .env.local, installed the necessary package, configured access to upload data to Cloudinary, and stored the image URL in the database.

--- actions/create-post.js
+++ actions/create-post.js
@@ -2,6 +2,7 @@
 
 import { redirect } from 'next/navigation.js';
 import { storePost } from '@/lib/posts';
+import { uploadImage } from '@/lib/cloudinary.js';
 
 export async function createPost(prevState, formData) {
     const title = formData.get('title');
@@ -11,27 +12,35 @@ export async function createPost(prevState, formData) {
     let errors = [];
 
     if (!title || title.trim() === '') {
-       errors.push('Title is required!');
+        errors.push('Title is required!');
     }
 
     if (!content || content.trim() === '') {
-      errors.push('Content is required!');
+        errors.push('Content is required!');
     }
 
     if (!image || image.size === 0) {
-      errors.push('Image is required!');
+        errors.push('Image is required!');
     }
 
     if (errors.length > 0) {
-      return { errors };
+        return { errors };
+    }
+
+    let imageUrl;
+
+    try {
+        imageUrl = await uploadImage(image);
+    } catch (err) {
+        throw new Error('Image upload failed, please try again later.')
     }
 
     await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
+        imageUrl,
+        title,
+        content,
+        userId: 1
     })
 
     redirect('/feed');
-  }
+}
