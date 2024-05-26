dff3d6a3dd7be0c90bb43f11bc1f1b19ebde38ff
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-26 16:09:33 +0300

    Used the useOptimistic hook for a better experience so that it immediately displays the result of the user interaction while the database query is being executed.

--- components/posts.js
+++ components/posts.js
@@ -1,8 +1,12 @@
+'use client';
+
+import { useOptimistic } from 'react';
+
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 import { toggleLike } from '@/actions/like-post.js';
 
-function Post({ post }) {
+function Post({ post, action }) {
 
   return (
     <article className="post">
@@ -21,8 +25,8 @@ function Post({ post }) {
             </p>
           </div>
           <div>
-            <form action={toggleLike.bind(null, post.id)} className={post.isLiked ? 'liked' : ''}>
-              <LikeButton/>
+            <form action={action.bind(null, post.id)} className={post.isLiked ? 'liked' : ''}>
+              <LikeButton />
             </form>
           </div>
         </header>
@@ -33,15 +37,42 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, postId) => {
+    const index = prevPosts.findIndex(p => p.id === postId);
+
+    if (index === -1) {
+      return prevPosts;
+    }
+
+    const updatedPost = Object.assign({}, prevPosts[index]);
+    updatedPost.isLiked = !updatedPost.isLiked;
+
+    if (updatedPost.isLiked) {
+      updatedPost.likes -= 1;
+    } else {
+      updatedPost.likes += 1;
+    }
+
+    const newPostsArray = [...prevPosts];
+    newPostsArray.splice(index, 1, updatedPost);
+    return newPostsArray;
+  });
+
+  async function updatePost(postId) {
+    updateOptimisticPosts(postId);
+    await toggleLike(postId);
+  }
+
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatePost} />
         </li>
       ))}
     </ul>
