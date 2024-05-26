7892f798259dc7cce077b23c5379a68171fe3510
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-26 22:15:10 +0300

    Updated revalidate paths for all pages, not just '/feed'.

--- actions/create-post.js
+++ actions/create-post.js
@@ -43,6 +43,6 @@ export async function createPost(prevState, formData) {
         content,
         userId: 1
     })
-    revalidatePath('/feed', 'page');
+    revalidatePath('/', 'page');
     redirect('/feed');
 }
