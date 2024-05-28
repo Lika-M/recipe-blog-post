0cb4d03c797bfdbb2c91281c8bfe94a682038883
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-28 11:26:00 +0300

    Replaced the standard <img> with the Image component of Next.js. Additional attributes have been added: priorities in the Header, fill in the Post component, and the container size has been set.

--- components/header.js
+++ components/header.js
@@ -1,11 +1,19 @@
-import logo from '@/assets/logo.png';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import logo from '@/assets/logo.png';
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          priority
+          quality={50} //{number 1-100}
+          sizes="10vw"
+          alt="Mobile phone with posts feed on it"
+        />
       </Link>
       <nav>
         <ul>
